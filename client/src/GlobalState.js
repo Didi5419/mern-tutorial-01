018f5fe3a089ad60a20ac07d33f6cd9098b40573
didi1954 <72092342+Didi5419@users.noreply.github.com>
2020-10-01 16:34:46 +0200

    Add files via upload

---
+++ client/src/GlobalState.js
@@ -0,0 +1,31 @@
+import React, { createContext, useState, useEffect } from "react";
+import ProductsAPI from "./api/ProductsAPI";
+import UserAPI from "./api/UserAPI"; // Film 12  (03:23)
+
+import axios from "axios";
+
+export const GlobalState = createContext();
+
+export const DataProvider = ({ children }) => {
+  const [token, setToken] = useState(false);
+
+  const refreshToken = async () => {
+    const res = await axios.get("/user/refresh_token");
+
+    setToken(res.data.accesstoken);
+  };
+
+  useEffect(() => {
+    const firstLogin = localStorage.getItem("firstLogin");
+    if (firstLogin) refreshToken();
+  }, []);
+
+  const state = {
+    token: [token, setToken],
+    productsAPI: ProductsAPI(),
+    userAPI: UserAPI(token),
+  };
+
+  return <GlobalState.Provider value={state}>{children}</GlobalState.Provider>;
+};
+// Film 11  23:48    Film 12     03:31 - 06:50
